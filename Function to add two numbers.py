bc6e1f717f2198258a80994228ed4471178f265d
Niru Thapa <133076588+Niruthapa@users.noreply.github.com>
2025-04-17 22:05:08 +0545

    Add files via upload

---
+++ Function to add two numbers.py
@@ -0,0 +1,10 @@
+# Function to add two numbers
+def add_numbers(a, b):
+    result = a + b
+    return result
+
+# Call the function and store the result
+sum_result = add_numbers(5, 10)
+
+# Display the result
+print("The sum is:", sum_result)
